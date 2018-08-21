8b0f019ce74f942b67ec0989beb1e30964757098
Andrew Park <apark11278@gmail.com>
2018-08-21 09:37:13 -0400

    Changed connection credentials

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection(
     process.env.JAWSDB_URL || {
-        host: process.env.MYSQL_HOST || "localhost",
-        port: process.env.MYSQL_PORT || 8889,
-        user: process.env.MYSQL_USER || "root",
-        password: process.env.MYSQL_SECRET || "root",
-        database: process.env.MYSQL_DB || "burgers_db"
+        host: process.env.MYSQL_HOST || "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+        port: process.env.MYSQL_PORT || 3306,
+        user: process.env.MYSQL_USER || "flwggm2f0cyn1sob",
+        password: process.env.MYSQL_SECRET || "uv2yc7rowoz02hoh",
+        database: process.env.MYSQL_DB || "hk9e06vgypastvk8"
     }
 );
 
