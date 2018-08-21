9924013276e64790f68d979f32552f589c668740
Andrew Park <apark11278@gmail.com>
2018-08-21 09:31:36 -0400

    Added jawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,14 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST || "localhost",
-    port: process.env.MYSQL_PORT || 8889,
-    user: process.env.MYSQL_USER || "root",
-    password: process.env.MYSQL_SECRET || "root",
-    database: process.env.MYSQL_DB || "burgers_db"
-});
+var connection = mysql.createConnection(
+    process.env.JAWSDB_URL || {
+        host: process.env.MYSQL_HOST || "localhost",
+        port: process.env.MYSQL_PORT || 8889,
+        user: process.env.MYSQL_USER || "root",
+        password: process.env.MYSQL_SECRET || "root",
+        database: process.env.MYSQL_DB || "burgers_db"
+    }
+);
 
 connection.connect(function (err) {
     if (err) {
