2aa47e647081fe74b790afb972c1c79a58210fa4
Andrew Park <apark11278@gmail.com>
2018-08-21 00:41:38 -0400

    Please work

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,7 +5,7 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 
-router.get("/", function(req, res){
+router.get("*", function(req, res){
     burger.all(function(data){
         var hbsObject = {
             burgers: data
