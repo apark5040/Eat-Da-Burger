6e5b6c24b1716ba8446570430b046583a7d31192
Andrew Park <apark11278@gmail.com>
2018-08-21 00:13:17 -0400

    Fixing heroku deploy

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ var app = express();
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 var PORT = process.env.PORT || 3000;
 
@@ -30,5 +30,5 @@ app.use(routes);
 // Start our server so that it can begin listening to client requests.
 app.listen(PORT, function () {
     // Log (server-side) when our server has started
-    console.log("Server listening on: http://localhost:" + PORT);
+    console.log("Server listening on port: " + PORT);
 });
